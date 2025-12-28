666015fe52d7994fe058d406546ec72e7186c259
Aravind Murugaraj <aravind_murugaraj@epam.com>
2025-12-28 23:58:51 +0530

    Practical Task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import SearchForm from './components/SearchForm';
+import GenreSelect from './components/GenreSelect';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState('ALL'); // default genre
+  const genres = ['ALL', 'DOCUMENTARY', 'COMEDY', 'HORROR', 'CRIME'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Applying background color
+    <div style={{ backgroundColor: '#f5f5f5', minHeight: '100vh', padding: '40px' }}>
+      <div style={{ marginBottom: '60px' }}>
+        <h2 style={{ textAlign: 'center' }}>Counter</h2>
+        <Counter initialValue={0} />
+      </div>
+
+      <div style={{ marginBottom: '60px' }}>
+        <h2 style={{ textAlign: 'center' }}>Search Form</h2>
+        <SearchForm
+          initialQuery=""
+          onSearch={q => {
+            setSearch(q);
+          }}
+        />
+      </div>
+
+      <div style={{ marginBottom: '60px' }}>
+        <h2 style={{ textAlign: 'center' }}>Genre Select</h2>
+        <GenreSelect
+          genres={genres}
+          selectedGenre={selectedGenre}
+          onSelect={setSelectedGenre}
+        />
+      </div>
     </div>
   );
 }
